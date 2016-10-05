a4aeb9d30089a754c478f9f8442bb3e41ca18ac5
Armando <armando.santisbon@gmail.com>
2016-10-05 00:40:43 -0500

    Add, subtract, multiply by scalar

--- test.py
+++ test.py
@@ -21,6 +21,23 @@ class TestVectors(unittest.TestCase):
         self.assertTrue(vector1 == vector2)
         self.assertFalse(vector1 == vector3)
 
+    def test_add(self):
+        vector1 = Vector([8, -9])
+        vector2 = Vector([-1, 2])
+
+        self.assertEqual(vector1.add(vector2), Vector([7, -7]))
+
+    def test_subtract(self):
+        vector1 = Vector([8, -9])
+        vector2 = Vector([-1, 2])
+
+        self.assertEqual(vector1.subtract(vector2), Vector([9, -11]))
+
+    def test_scalar_multiply(self):
+        vector1 = Vector([9, -4, 7])
+
+        self.assertEqual(vector1.scalar_multiply(3), Vector([27, -12, 21]))
+
 # One way to run the tests from the comnad line
 # if __name__ == '__main__':
 #     unittest.main()
--- vector.py
+++ vector.py
@@ -13,7 +13,55 @@ class Vector(object):
             raise TypeError('The coordinates must be an iterable')
 
     def __str__(self):
+        # {} implicitly references the first positional argument
         return 'Vector: {}'.format(self.coordinates)
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+
+    def add(self, v):
+        result = []
+
+        # Using a for loop
+        # if self.dimension == v.dimension:
+        #     for i in range(self.dimension):
+        #         result.append(self.coordinates[i] + v.coordinates[i])
+        # else:
+        #     raise ValueError('Vectors must have the same # of dimensions.')
+
+        # Using a list comprehension
+        if self.dimension == v.dimension:
+            result = [x + y for x, y in zip(self.coordinates, v.coordinates)]
+        else:
+            raise ValueError('Vectors must have the same # of dimensions.')
+
+        return Vector(result)
+
+    def subtract(self, v):
+        result = []
+
+        # Using a for loop
+        # if self.dimension == v.dimension:
+        #     for i in range(self.dimension):
+        #         result.append(self.coordinates[i] - v.coordinates[i])
+        # else:
+        #     raise ValueError('Vectors must have the same # of dimensions.')
+
+        # Using a list comprehension
+        if self.dimension == v.dimension:
+            result = [x - y for x, y in zip(self.coordinates, v.coordinates)]
+        else:
+            raise ValueError('Vectors must have the same # of dimensions.')
+
+        return Vector(result)
+
+    def scalar_multiply(self, c):
+        # Using a for loop
+        # result = []
+        # for i in range(self.dimension):
+        #     result.append(self.coordinates[i] * c)
+
+        # Using a list comprehension
+        result = [x * c for x in self.coordinates]
+
+        return Vector(result)
